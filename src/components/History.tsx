fd40bcd442ac6017b32de089c78a6de7d2ecdbdb
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-16 13:35:37 +0200

    feat: implement the name and the dropdown elements to show

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,5 +1,26 @@
-import React from 'react'
+export default function History({ history }: any) {
+  let height: number = 30;
+  return (
+    <div
+      className={`h-[${height}%] w-[90%] items-center justify-center rounded-xl bg-gray-900`}
+    >
+      <h1 className="pt-5 text-center text-3xl text-amber-600">History</h1>
+      <h2 className="pt-5 text-center text-3xl text-emerald-700">
+        Founded in {history.founded.year} by {history.founded.by}
+      </h2>
 
-export default function History() {
-  return <div>History component</div>
+      <form>
+        <select
+          className="items-center justify-center text-center text-3xl text-amber-600"
+          value={history.majorEvents}
+          multiple={false}
+        >
+          <option value={""}>Select a Year</option>
+          {history.majorEvents.map((item) => {
+            return <option value={item.year}>{item.year}</option>;
+          })}
+        </select>
+      </form>
+    </div>
+  );
 }
