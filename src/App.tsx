d63dd9d445cbc9cddd819aeb97baeab04b2ad41b
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-16 09:13:35 +0200

    feat: implement the call to retun the json data and insert it as state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import CitadelItem from "./interface";
 
 function App() {
+  const [data, setData] = useState<CitadelItem | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("/citadel.json");
+      const response = await data.json();
+      setData(response);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Citadel of Eternal Night</h1>
       <p>Construye aquí la interfaz y lógica de la ciudadela.</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
