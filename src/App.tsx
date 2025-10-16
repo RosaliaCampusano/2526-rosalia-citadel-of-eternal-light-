25712d48e84054fc68b66e240bc8c037bc343c34
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-16 09:56:48 +0200

    feat: implement the logic to display the actual screen when navigation detect an event onclick

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React, { useState, useEffect } from "react";
 import CitadelItem from "./interface";
+import { useState, useEffect } from "react";
+import Navigation from "./helpers/Navigation";
+import { NavigationScreen } from "./constants";
+import History from "./components/History";
+import Defenses from "./components/Defenses";
+import Location from "./components/Location";
+import Resources from "./components/Resources";
+import Inhabitants from "./components/Inhabitants";
 
 function App() {
   const [data, setData] = useState<CitadelItem | null>(null);
+  const [screen, setScreen] = useState<NavigationScreen>(
+    NavigationScreen.Location,
+  );
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,11 +24,26 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>Citadel of Eternal Night</h1>
-      <p>Construye aquí la interfaz y lógica de la ciudadela.</p>
-    </div>
+    <>
+      <Navigation setScreen={setScreen} />
+      <>{handlerScreen(screen)}</>
+    </>
   );
 }
+
+const handlerScreen = (screen: NavigationScreen) => {
+  switch (screen) {
+    case NavigationScreen.Location:
+      return <Location />;
+    case NavigationScreen.Defense:
+      return <Defenses />;
+    case NavigationScreen.History:
+      return <History />;
+    case NavigationScreen.Inhabitants:
+      return <Inhabitants />;
+    case NavigationScreen.Resources:
+      return <Resources />;
+  }
+};
 
 export default App;
---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+enum NavigationScreen {
+  Location = 0,
+  Defense = 1,
+  Inhabitants = 2,
+  Resources = 3,
+  History = 4,
+}
+
+const NavigationName = [
+  "Location",
+  "Defenses",
+  "History",
+  "Resources",
+  "Inhabitants",
+];
+
+export { NavigationScreen, NavigationName };
