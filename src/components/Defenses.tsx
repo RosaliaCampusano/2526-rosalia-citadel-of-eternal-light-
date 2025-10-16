8db6cf07fc9c772ec135e9c828b007c404bdb2af
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-16 13:36:18 +0200

    feat: implement the component Defense showing the card and progress attribute

--- src/components/Defenses.tsx
+++ src/components/Defenses.tsx
@@ -1,5 +1,40 @@
-import React from 'react'
+import { Card } from "../helpers/Card";
 
-export default function Defenses() {
-  return <div>Defenses component</div>
+export default function Defenses({ defense }) {
+  return (
+    <div className="h-full w-full items-center justify-center rounded-xl bg-gray-900">
+      <h1 className="text-2 pt-5 text-center text-amber-600">
+        Citadel Defenses
+      </h1>
+      <p className="text-1xl pt-5 text-center text-amber-50">Walls</p>
+      <p className="text-1xl pt-5 text-center text-amber-50">
+        Materials: {defense.walls.material}
+      </p>
+      <p className="text-1xl pt-5 text-center text-amber-50">
+        Height: {defense.walls.height}
+      </p>
+      <h1 className="pt-5 text-center text-2xl text-amber-500">Enchantments</h1>
+      {defense.walls.enchantments.map((elem) => {
+        let level: number = Math.floor((elem.level * 50) / 50);
+        return (
+          <>
+            <p className="text-1xl ml-20 pt-5 text-left text-amber-50">
+              {elem.name} ({elem.level})
+            </p>
+            <div className="ml-10 h-[2%] w-[50%] items-center justify-center bg-gray-500">
+              <div
+                className={`h-[100%] w-[${level}%] items-center justify-center bg-blue-400`}
+              ></div>
+            </div>
+            ;
+          </>
+        );
+      })}
+      <div className="ml-10 h-[1%] w-[90%] items-center justify-center bg-gray-500"></div>
+      <h1 className="pt-5 text-center text-2xl text-amber-500">Towers</h1>
+      {defense.towers.map((elem) => {
+        return <Card tower={elem} />;
+      })}
+    </div>
+  );
 }
---
+++ src/helpers/Card.tsx
@@ -0,0 +1,41 @@
+export const Card = ({ tower }) => {
+  let progressBarColor: string = "";
+  const porcentBar = Math.floor((tower.armament.ammunitionCount * 1000) / 100);
+
+  if (porcentBar <= 250) {
+    progressBarColor = "bg-red-500";
+  } else if (porcentBar <= 500 && porcentBar > 250) {
+    progressBarColor = "bg-amber-500";
+  } else if (porcentBar <= 1000 && porcentBar > 500) {
+    progressBarColor = "bg-emerald-500";
+  }
+
+  return (
+    <div className="mt-10 h-[50%] w-[50%] items-center justify-center rounded-xl bg-blue-500">
+      <p className="ml-20 pt-5 text-center text-3xl text-amber-50">
+        {tower.name}
+      </p>
+      <p className="ml-20 text-center text-3xl text-amber-50">
+        Height: {tower.height}
+      </p>
+      <p className="ml-20 text-center text-3xl text-amber-500">Armament</p>
+      <p className="ml-20 text-center text-3xl text-red-500">
+        {tower.armament.weaponType} - {tower.armament.ammunitionType} -{" "}
+        {tower.armament.ammunitionCount}
+      </p>
+      <div className="ml-10 h-[2%] w-[100%] items-center justify-center bg-gray-500">
+        <div
+          className={`h-[100%] w-[${porcentBar - 100}%] items-center justify-center ${progressBarColor}`}
+        ></div>
+      </div>
+      <h1 className="pt-5 text-center text-4xl text-amber-600">Guards</h1>
+      {tower.guards.map((elem) => {
+        return (
+          <p className="pt-5 text-center text-3xl text-amber-50">
+            {elem.name} - {elem.rank}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
